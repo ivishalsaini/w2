d7eca4d40c2ef8536970bcca9be5577d49246cd0
Vishal Saini <vishalv1217@gmail.com>
2020-07-04 01:06:13 +0530

    Updates

---
+++ precache-manifest.908fa15927ad043119cadbbe6590fb57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe363e765781651ab9d3",
+    "url": "/w2/static/css/main.9e3150d0.chunk.css"
+  },
+  {
+    "revision": "fe363e765781651ab9d3",
+    "url": "/w2/static/js/main.fe363e76.chunk.js"
+  },
+  {
+    "revision": "ae73830a415787622070",
+    "url": "/w2/static/css/1.8421ef19.chunk.css"
+  },
+  {
+    "revision": "ae73830a415787622070",
+    "url": "/w2/static/js/1.ae73830a.chunk.js"
+  },
+  {
+    "revision": "a11501e5f7778588263f",
+    "url": "/w2/static/js/runtime~main.a11501e5.js"
+  },
+  {
+    "revision": "4a3d3f4a124d0bba579fb8b4352336fa",
+    "url": "/w2/index.html"
+  }
+];
